e0629f55b580984611e203b1c825af8d7c64bca1
yuki-hata1925 <69428254+elan0@users.noreply.github.com>
2021-07-20 21:21:03 +0900

    POPOPO

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeemanagementApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeemanagementApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
